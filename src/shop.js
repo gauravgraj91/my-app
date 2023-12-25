ddb792215e353a712a6c6ec3436d19dea659e30f
Gaurav Raj <gauravraj@Gauravs-MacBook-Air.local>
2023-12-25 22:45:31 +0530

    Added Data Grid support for shop section.

--- src/shop.js
+++ src/shop.js
@@ -1,22 +1,61 @@
 import React from 'react';
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
+import { DataGrid } from '@mui/x-data-grid';
 
 const Shop = () => {
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 90,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 }
+  ];
+
   return (
     <div>
       <div>
         <TextField 
-          label="Enter text..." 
+          label="Enter Product Name..." 
           variant="outlined" 
         />
-        <Button variant="contained">Save</Button>
         <TextField 
-          label="Enter more text..." 
+          label="Enter MRP of the Product..." 
           variant="outlined" 
         />
         <Button variant="contained">Save</Button>
       </div>
+      <div style={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+        </div>
     </div>
   );
 };
